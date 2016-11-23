b04957818dfcf61cfaf1d8ca6e92d6f0a059651d
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-11-22 20:29:58 -0500

    added more files

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('default', function() { console.log("Is this the real life?");
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("styles/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("styles/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("styles/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
