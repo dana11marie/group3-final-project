76164092f2621233ca0c0f8a7a7df8dbf1934283
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-11-22 21:23:44 -0500

    rating test

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 // SAMPLE APP.JS
+//
+// var app = angular.module('myModule', ['ngRoute']);
+//
+// app.config(function($routeProvider){
+//   $routeProvider
+//   .when('/view1', {
+//     controller: 'ctrlOne',
+//     templateUrl: 'partials/view1.html'
+//   })
+//   .when('/view2', {
+//     controller: 'ctrlTwo',
+//     templateUrl: 'partials/view2.html'
+//   })
+//   .otherwise({
+//     redirectTo: '/'
+//   });
+// });
 
-var app = angular.module('myModule', ['ngRoute']);
+// CONTROL ONE:
 
-app.config(function($routeProvider){
-  $routeProvider
-  .when('/view1', {
-    controller: 'ctrlOne',
-    templateUrl: 'partials/view1.html'
-  })
-  .when('/view2', {
-    controller: 'ctrlTwo',
-    templateUrl: 'partials/view2.html'
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
+var app = angular.module('movieMod', []);
+
+app.controller('movieCtrl', function($http, $scope) {
+
+  $scope.findMovie = function(rating) {
+
+// rating is populating as undefined.
+
+    $http.get('https://api.themoviedb.org/3/discover/movie?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US&sort_by=vote_average.asc&include_adult=false&vote_average.gte=' + rating + '&with_runtime.lte=180')
+    .then(function successCallback(response) {
+
+      var movieData = response;
+      console.log(movieData);
+
+      $scope.runTime = movieData.data.runtime;
+
+    });
+
+  };
+
 });
--- js/ctrl1.js
+++ js/ctrl1.js
@@ -1,19 +1,19 @@
-var app = angular.module('movieMod', []);
-
-app.controller('movieCtrl', function($http, $scope) {
-
-  $scope.findMovie = function(movieTitle) {
-
-    $http.get('http://netflixroulette.net/api/api.php?title=' + movieTitle)
-    .then(function successCallback(response) {
-
-      var movieData = response;
-      console.log(movieData.data);
-
-      $scope.runTime = movieData.data.runtime;
-
-    });
-
-  };
-
-});
+// var app = angular.module('movieMod', []);
+//
+// app.controller('movieCtrl', function($http, $scope) {
+//
+//   $scope.findMovie = function(movieTitle) {
+//
+//     $http.get('https://api.themoviedb.org/3/discover/movie?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=true&vote_average.gte=' + rating + '&with_genres=' + genre + '&with_runtime.lte=' + runtime)
+//     .then(function successCallback(response) {
+//
+//       var movieData = response;
+//       console.log(movieData.data);
+//
+//       $scope.runTime = movieData.data.runtime;
+//
+//     });
+//
+//   };
+//
+// });
