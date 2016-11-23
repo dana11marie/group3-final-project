fa54931503fddd43a9cc270df95e3a286f656585
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-11-23 16:53:22 -0500

    added genre, years, runtime

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ var app = angular.module('movieMod', []);
 
 app.controller('movieCtrl', function($http, $scope) {
 
-  $scope.findMovie = function(releaseYear, rating) {
+  $scope.findMovie = function(genre, rating, startYear, endYear, runtime) {
 
 // img needs to be added to the end of https://image.tmdb.org/t/p/w500
 
-    $http.get('https://api.themoviedb.org/3/discover/movie?api_key=246abf971b8f4d88b4c901eeacc07819&sort_by=popularity.desc&include_adult=false&primary_release_date.gte=' + releaseYear +'&vote_average.gte=' + rating +'&with_runtime.lte=180')
+    $http.get('https://api.themoviedb.org/3/discover/movie?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&primary_release_date.gte=' + startYear + '&primary_release_date.lte=' + endYear + '&vote_average.gte=' + rating + '&without_genres=' + genre + '&with_runtime.lte=' + runtime)
     .then(function successCallback(response) {
 
       var movieData = response;
