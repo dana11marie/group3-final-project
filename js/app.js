71ac2c2a3b1af23ffa49b28d43c50c87e5c0b8d9
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-11-27 10:25:30 -0500

    setup view1/ctrl1

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('movieMod', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    controller: 'movieCtrl',
+    templateUrl: 'partials/view1.html'
+  });
+});
