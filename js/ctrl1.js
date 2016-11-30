f11b8e3cdc45e7aec7ad6e063496ac1875853c5c
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-11-30 18:02:03 -0500

    fixed js

--- js/ctrl1.js
+++ js/ctrl1.js
@@ -5,22 +5,19 @@ app.controller('movieCtrl', function($http, $scope) {
   $scope.findMovie = function(genre, rating, startYear, endYear, runtime) {
 
     $http.get('https://api.themoviedb.org/3/discover/movie?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&primary_release_date.gte=' + startYear + '&primary_release_date.lte=' + endYear + '&vote_average.gte=' + rating + '&with_genres=' + genre + '&with_runtime.lte=' + runtime + '&page=1')
-    .then(function (response) {
+      .then(function successCallback(response) {
+        $scope.movies = response.data.results;
 
-
-
-      var movies = response.data.results;
-
-      movies.forEach(function(movie){
-
-        $http.get('https://api.themoviedb.org/3/movie/' + movie.id + '/videos?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US')
-           .then(function (response) {
-             //movie.trailerkey = response[0].trailerkey;
-             console.log(response);
-      });
-      console.log($scope.movies);
-    });
-  });
+        // movies.forEach(function(movie){
+        //
+        //     $http.get('https://api.themoviedb.org/3/movie/' + movie.id + '/videos?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US')
+        //        .then(function (response) {
+        //        //movie.trailerkey = response[0].trailerkey;
+        //          console.log(response);
+        //        });
+        //        console.log($scope.movies);
+        //     });
+        });
 
 
   // movieId = movie.id;
@@ -38,9 +35,6 @@ app.controller('movieCtrl', function($http, $scope) {
 
 
 
-
-
-
-
+  };
 
 });
