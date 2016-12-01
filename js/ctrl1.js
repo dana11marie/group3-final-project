7bc175ed147b2ac4fedc0cbabd1017bf56d44eaf
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-11-30 19:24:19 -0500

    got trailers working

--- js/ctrl1.js
+++ js/ctrl1.js
@@ -5,18 +5,26 @@ app.controller('movieCtrl', function($http, $scope) {
   $scope.findMovie = function(genre, rating, startYear, endYear, runtime) {
 
     $http.get('https://api.themoviedb.org/3/discover/movie?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&primary_release_date.gte=' + startYear + '&primary_release_date.lte=' + endYear + '&vote_average.gte=' + rating + '&with_genres=' + genre + '&with_runtime.lte=' + runtime + '&page=1')
-      .then(function successCallback(response) {
-        $scope.movies = response.data.results;
+      .then(function successCallback(responseOne) {
 
-        // movies.forEach(function(movie){
-        //
-        //     $http.get('https://api.themoviedb.org/3/movie/' + movie.id + '/videos?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US')
-        //        .then(function (response) {
-        //        //movie.trailerkey = response[0].trailerkey;
-        //          console.log(response);
-        //        });
-        //        console.log($scope.movies);
-        //     });
+        var movies = responseOne.data.results;
+
+         $scope.movies = movies;
+         $scope.quantity = 6;
+         console.log($scope.movies);
+
+
+        // REQUEST FOR YOUTUBE KEY
+        movies.forEach(function(movie){
+            $http.get('https://api.themoviedb.org/3/movie/' + movie.id + '/videos?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US')
+               .then(function successCallback(responseTwo) {
+               movie.trailerkey = responseTwo.data.results[0].key;
+                 //console.log(movie.trailerkey);
+               });
+              // console.log($scope.movies);
+            });
+        //   // END REQUEST FOR YOUTUBE KEY
+
         });
 
 
