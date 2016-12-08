d36409e8505d39f8c463b345878bef143b1df962
Jade Rubino <jade.gesinski@gmail.com>
2016-12-07 20:12:16 -0500

    Dana & Jade changes 8pm

--- js/ctrl1.js
+++ js/ctrl1.js
@@ -4,9 +4,9 @@ var app = angular.module('movieMod');
 // CONTROLLER
 app.controller('movieCtrl', function($http, $scope) {
 
-  $scope.findMovie = function(genre, rating, startYear, endYear, runtime) {
+  $scope.findMovie = function(type, genre, rating, startYear, endYear, runtime) {
     // MOVIES REQUEST BASED ON FILTERS
-    $http.get('https://api.themoviedb.org/3/discover/movie?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&primary_release_date.gte=' + startYear + '&primary_release_date.lte=' + endYear + '&vote_average.gte=' + rating + '&with_genres=' + genre + '&with_runtime.lte=' + runtime )
+    $http.get('https://api.themoviedb.org/3/discover/'+ type +'?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&primary_release_date.gte=' + startYear + '&primary_release_date.lte=' + endYear + '&vote_average.gte=' + rating + '&with_genres=' + genre + '&with_runtime.lte=' + runtime + '&first_air_date.gte=' + startYear + '&first_air_date.lte='+ endYear )
         .then(function successCallback(responseOne) {
           //STORE RESPONSE AS VARIABLE
           var movies = responseOne.data.results;
@@ -27,18 +27,16 @@ app.controller('movieCtrl', function($http, $scope) {
            });
         // END REQUEST FOR YOUTUBE KEY
 
+                // REQUEST FOR ADDITIONAL DETAILS
+               movies.forEach(function(movie){
+                  $http.get('https://api.themoviedb.org/3/'+ type +'/'+ movie.id +'?api_key=246abf971b8f4d88b4c901eeacc07819&language=en-US&append_to_response=runtime')
+                       .then(function successCallback(responseThree) {
+                       movie.runtime = responseThree.data.runtime;
+                       });
+                  });
+               // END REQUEST FOR ADDITIONAL DETAILS
 
-/*         // TOGGLE MOVIE DESCRIPTION
-        $(document).ready(function(){
-          $('.summary').hide();
-          $('.button').click(function(){
-            $(this).parent().children(".summary").toggle();
-          });
-        });
-        // END TOGGLE MOVIE DESCRIPTION */
-
-
-        });
+      });
 
     };
 
