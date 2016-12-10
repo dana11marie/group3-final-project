4d79eefb522bc2d921f1784f8bff0515ef0c556f
Jade Rubino <jade.gesinski@gmail.com>
2016-12-10 13:38:09 -0500

    Popup Contact Form Added to Footer

--- js/ctrl1.js
+++ js/ctrl1.js
@@ -53,3 +53,22 @@ app.controller('movieCtrl', function($http, $scope) {
 
 
 });
+//START POPUP CONTACT FORM
+$(function() {
+
+  // contact form animations
+  $('#contactUs').click(function() {
+    $('#contactUsForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactUsForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+
+});
+//END POPUP CONTACT FORM
